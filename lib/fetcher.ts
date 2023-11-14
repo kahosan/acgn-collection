7241cff4e054c33e2c6275cbfd2caea6d9562230
kahosan <15116355657l@gmail.com>
2023-11-14 16:55:43 +0800

    chore: remove useless param

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -25,45 +25,37 @@ export const fetcherErrorHandler = (error: Error, message?: string) => {
   }
 };
 
-export async function fetcher<T>(key: string | [string, string | RequestInit], { arg }: { arg?: Record<string, any> } = {}, options?: RequestInit) {
+export async function fetcher<T>(key: string | [string, string | RequestInit], { arg }: { arg?: Record<string, any> } = {}) {
   const headers = new Headers();
 
   const [url] = typeof key === 'string' ? [key] : key;
 
   let token: string | undefined;
-  let keyOptions: RequestInit | undefined;
+  const options: RequestInit = {};
 
   const v = Array.isArray(key) ? key.at(1) : undefined;
   if (typeof v === 'string')
     token = v;
   else if (v && typeof v !== 'string')
-    keyOptions = v;
+    Object.assign(options, v);
 
   if (token)
     headers.set('Authorization', `Bearer ${token}`);
 
-  if (keyOptions?.headers) {
-    for (const [key, value] of Object.entries(keyOptions.headers))
-      headers.set(key, value);
-  } else if (options?.headers) {
+  if (options.headers) {
     for (const [key, value] of Object.entries(options.headers))
       headers.set(key, value);
   }
 
-  const init = {
-    ...options,
-    ...keyOptions
-  };
-
   if (arg) {
-    init.method = 'POST';
-    init.body = JSON.stringify(arg);
+    options.method = 'POST';
+    options.body = JSON.stringify(arg);
     headers.set('Content-Type', 'application/json');
   }
 
   const res = await fetch(new URL(url, BASE_URL), {
     headers,
-    ...init
+    ...options
   });
 
   const data = res.headers.get('Content-Type')?.includes('application/json') ? await res.json() : await res.text();
