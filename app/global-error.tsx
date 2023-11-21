e5c5e76231e16e63cdd1a45064387a36c4cca17c
kahosan <15116355657l@gmail.com>
2023-11-21 12:40:40 +0800

    fix: name

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -24,7 +24,7 @@ export default function GlobalError({ error }: { error: Error }) {
             <h2 className="font-bold text-2xl text-blue-300 mb-2">
               嘟嘟噜
               <br />
-              凶真，网页坏掉啦!
+              冈伦，网页坏掉啦!
             </h2>
             <div className="opacity-75">
               报错信息在这里哦：<p className="font-mono break-words break-all">{error instanceof HTTPError ? error.data.description : error.message}</p>
