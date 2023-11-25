73458f9be34c2965cba14a1f9139e1ec0437905f
kahosan <15116355657l@gmail.com>
2023-11-25 15:52:35 +0800

    fix(auth): loop

--- app/login/form.tsx
+++ app/login/form.tsx
@@ -4,9 +4,14 @@ import { Button } from '@nextui-org/react';
 
 import { signIn } from 'next-auth/react';
 import { useSearchParams } from 'next/navigation';
+import { toast } from 'sonner';
 
 export default function LoginForm() {
   const searchParams = useSearchParams();
+  const error = searchParams.get('error');
+
+  if (error)
+    toast.error(`登入错误: ${error}`, { id: 'login-error' });
 
   const login = () => {
     const callbackUrl = searchParams.get('callbackUrl') || '/';
--- middleware.ts
+++ middleware.ts
@@ -6,12 +6,18 @@ import type { NextRequest } from 'next/server';
 
 import { refreshAccessToken } from './lib/auth';
 
-function signOut(request: NextRequest) {
-  const response = NextResponse.redirect(new URL('/login', request.url));
+function secureCheck(cookie: string) {
+  return cookie.startsWith('__Secure') || cookie.startsWith('__Host');
+}
+
+function signOut(request: NextRequest, message?: string) {
+  const response = NextResponse.redirect(new URL(`/login${message ? `?error=${message}` : ''}`, request.url));
 
   request.cookies.getAll().forEach(cookie => {
-    if (cookie.name.includes('next-auth.session-token'))
-      response.cookies.delete(cookie.name);
+    if (cookie.name.includes('next-auth')) {
+      const options = { maxAge: 0, secure: secureCheck(cookie.name) };
+      response.cookies.set(cookie.name, cookie.value, options);
+    }
   });
 
   return response;
@@ -21,37 +27,39 @@ const sessionCookie = process.env.NEXTAUTH_URL?.startsWith('https://')
   ? '__Secure-next-auth.session-token'
   : 'next-auth.session-token';
 
-export default withAuth(async req => {
-  const token = await getToken({ req });
+export default withAuth(
+  async req => {
+    const token = await getToken({ req });
 
-  if (!token) return signOut(req);
-  if (!token.expires) return signOut(req);
+    if (!token) return signOut(req, '获取 token 失败');
+    if (!token.expires) return signOut(req, 'token expires 不存在');
 
-  if (Date.now() < token.expires) return NextResponse.next();
+    if (Date.now() < token.expires) return NextResponse.next();
 
-  const _token = await refreshAccessToken(token);
-  if (!_token) return signOut(req);
+    const _token = await refreshAccessToken(token);
+    if (!_token) return signOut(req, '刷新 token 失败');
 
-  const session = await encode({
-    secret: process.env.NEXTAUTH_SECRET ?? '',
-    token: _token,
-    maxAge: (_token.expires - Date.now()) / 1000
-  });
+    const session = await encode({
+      secret: process.env.NEXTAUTH_SECRET ?? '',
+      token: _token,
+      maxAge: (_token.expires - Date.now()) / 1000
+    });
 
-  req.cookies.set(sessionCookie, session);
+    req.cookies.set(sessionCookie, session);
 
-  const response = NextResponse.next({ request: { headers: req.headers } });
-  response.cookies.set(sessionCookie, session);
+    const response = NextResponse.next({ request: { headers: req.headers } });
+    response.cookies.set(sessionCookie, session, { secure: secureCheck(sessionCookie) });
 
-  return response;
-},
-{
-  pages: {
-    signIn: '/login',
-    signOut: '/login',
-    error: '/login'
+    return response;
+  },
+  {
+    pages: {
+      signIn: '/login',
+      signOut: '/login',
+      error: '/login'
+    }
   }
-});
+);
 
 export const config = {
   matcher: '/((?!api|_next/static|_next/image|fonts|musume|placeholder|favicon.ico|login).*)'
