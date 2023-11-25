d4189b5fe8fa7015deb8b83184317666d18e139d
kahosan <15116355657l@gmail.com>
2023-11-25 18:31:36 +0800

    feat(relations): part

--- components/collection-info/relations.tsx
+++ components/collection-info/relations.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 import { Link } from '@nextui-org/react';
 
 import { RelationsSkeleton } from './skeleton';
 
-import { useMemo } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 
 import { useRelations } from '~/lib/bangumi/subjects';
 
@@ -17,6 +18,24 @@ interface Props {
 
 export default function Relations({ subjectId }: Props) {
   const { data, isLoading, error } = useRelations(subjectId);
+
+  const [showAll, setShowAll] = useState(false);
+  const [showButton, setShowButton] = useState(false);
+  const relationsRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new ResizeObserver(entries => {
+      for (const entry of entries) {
+        const scrollHeight = entry.target.scrollHeight;
+        setShowButton(scrollHeight > 432);
+      }
+    });
+
+    if (relationsRef.current)
+      observer.observe(relationsRef.current);
+
+    return () => observer.disconnect();
+  }, []);
 
   const relations = useMemo(() => {
     if (!data) return;
@@ -34,33 +53,53 @@ export default function Relations({ subjectId }: Props) {
   if (!relations || isLoading) return <RelationsSkeleton />;
 
   return (
-    <div className="grid grid-cols-[repeat(auto-fill,minmax(5rem,auto))] gap-4">
-      {
-        relations.map(([relation, items]) => {
-          return items.map((item, index) => (
-            <Link
-              color="foreground"
-              key={item.id}
-              className="flex-col items-start flex-auto"
-              href={`/subject/${item.id}`}
-            >
-              <div className="opacity-70 text-xs min-h-[15px]">{index === 0 ? relation : ''}</div>
-              <div className="relative p-2 rounded-md min-h-[5rem] w-full">
-                <Image
-                  src={item.images.small || 'https://placehold.co/64x64@3x.webp?text=No%20Image'}
-                  alt={item.name}
-                  className="object-cover bg-center transition-all opacity-0 duration-300 rounded-md"
-                  priority
-                  fill
-                  sizes="100%"
-                  onLoad={e => { e.currentTarget.style.opacity = '1'; }}
-                />
-              </div>
-              <p className="text-xs mt-1 line-clamp-2 break-all">{convertSpecialChar(item.name)}</p>
-            </Link>
-          ));
-        })
-      }
-    </div>
+    <>
+      <motion.div
+        initial={{ maxHeight: '27rem' }}
+        animate={{ maxHeight: showAll ? '1000rem' : '27rem' }}
+        className="grid grid-cols-[repeat(auto-fill,minmax(5rem,auto))] gap-4 overflow-hidden"
+        ref={relationsRef}
+      >
+        {
+          relations.map(([relation, items]) => {
+            return items.map((item, index) => (
+              <Link
+                color="foreground"
+                key={item.id}
+                className="flex-col items-start flex-auto"
+                href={`/subject/${item.id}`}
+              >
+                <div className="opacity-70 text-xs min-h-[15px]">{index === 0 ? relation : ''}</div>
+                <div className="relative p-2 rounded-md min-h-[5rem] w-full">
+                  <Image
+                    src={item.images.small || 'https://placehold.co/64x64@3x.webp?text=No%20Image'}
+                    alt={item.name}
+                    className="object-cover bg-center transition-all opacity-0 duration-300 rounded-md"
+                    priority
+                    fill
+                    sizes="100%"
+                    onLoad={e => { e.currentTarget.style.opacity = '1'; }}
+                  />
+                </div>
+                <p className="text-xs mt-1 line-clamp-2 break-all">{convertSpecialChar(item.name)}</p>
+              </Link>
+            ));
+          })
+        }
+      </motion.div>
+      <div className="text-right">
+        {
+          showButton
+            ? (
+              <span className="cursor-pointer text-sm dark:text-blue-200 text-blue-400" onClick={() => setShowAll(p => !p)}>
+                {
+                  showAll ? '显示部分' : '显示全部'
+                }
+              </span>
+            )
+            : null
+        }
+      </div>
+    </>
   );
 }
