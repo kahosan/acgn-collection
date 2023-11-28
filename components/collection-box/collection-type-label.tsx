673629700cb1a9060a8d1994bb9d34c97c9d4cd4
kahosan <15116355657l@gmail.com>
2023-11-28 17:17:11 +0800

    chore(collection-type-label): add shadow

--- components/collection-box/collection-type-label.tsx
+++ components/collection-box/collection-type-label.tsx
@@ -14,6 +14,7 @@ export default function CollectionTypeLabel({ userCollection }: Props) {
     <div
       className={clsx(
         'absolute left-[12px] top-3 py-0.5 px-2.5 rounded-sm rounded-tl-md rounded-br-md text-sm font-medium text-white',
+        'drop-shadow-[1px_1px_2px_black]',
         match(userCollection?.type)
           .with(1, () => 'bg-pink-500')
           .with(2, () => 'bg-fuchsia-500')
