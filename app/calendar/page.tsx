d3e0a7271a6c2aaa7329be0050c5d5c08b961a24
kahosan <15116355657l@gmail.com>
2023-11-28 15:59:32 +0800

    feat: improve calendar

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -1,12 +1,15 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import { Tab, Tabs } from '@nextui-org/react';
+import { DropdownItem, Tab, Tabs } from '@nextui-org/react';
 
 import Loading from '~/components/loading';
+import OptionsMenu from '~/components/options-menu';
 import CollectionCard from '~/components/collection-card';
 
-import { useMemo, useState } from 'react';
+import { useMemo } from 'react';
+import { match } from 'ts-pattern';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 import { useCalendar } from '~/lib/bangumi/subjects';
 
@@ -14,46 +17,90 @@ import { transformEnumToJSX } from '~/utils';
 
 import { Weekday } from '~/types/bangumi/calendar';
 
+const sortOptions = {
+  doing: '在看人数',
+  count: '评分人数',
+  rating: '社区评分'
+} as const;
+
+type SortKeys = keyof typeof sortOptions;
+
 export default function Calendar() {
   const { data, isLoading, error } = useCalendar();
 
+  const searchParams = useSearchParams();
+
   const today = new Date().getDay();
-  const [date, setDate] = useState(() => (today === 0 ? 7 : today));
+  const _day = searchParams.get('day');
+  const day = _day ? Number.parseInt(_day, 10) : (today === 0 ? 7 : today);
+
+  const sort = searchParams.get('sort') as SortKeys | null ?? 'doing';
+
+  const router = useRouter();
 
   const subjects = useMemo(() => {
-    return data?.find(({ weekday }) => weekday.id === date)?.items;
-  }, [data, date]);
+    return data?.find(({ weekday }) => weekday.id === day)
+      ?.items
+      .sort(
+        (a, b) => match(sort)
+          .with('doing', () => (b.collection?.doing ?? 0) - (a.collection?.doing ?? 0))
+          .with('count', () => (b.rating?.total ?? 0) - (a.rating?.total ?? 0))
+          .with('rating', () => (b.rating?.score ?? 0) - (a.rating?.score ?? 0))
+          .exhaustive()
+      );
+  }, [data, day, sort]);
 
   if (error) throw error;
 
   return (
-    <div>
-      <Tabs
-        aria-label="subject-type"
-        radius="sm"
-        variant="underlined"
-        selectedKey={date.toString()}
-        onSelectionChange={d => setDate(+d)}
-        classNames={{
-          base: 'w-full sm:w-auto mb-4',
-          tabList: 'w-full sm:w-auto',
-          tab: 'max-sm:h-7 max-sm:text-tiny max-sm:rounded-small',
-          cursor: 'max-sm:rounded-small'
-        }}
-      >
-        {
-          transformEnumToJSX((key, value) => (
-            <Tab key={key} title={value} />
-          ), Weekday)
-        }
-      </Tabs>
+    <>
+      <div className="flex flex-wrap sm:flex-nowrap justify-between items-baseline mb-4">
+        <Tabs
+          aria-label="subject-type"
+          radius="sm"
+          variant="underlined"
+          selectedKey={day.toString()}
+          onSelectionChange={d => router.push(`/calendar?day=${d}&sort=${sort}`)}
+          classNames={{
+            base: 'w-full sm:w-auto mb-2',
+            tabList: 'w-full sm:w-auto',
+            tab: 'max-sm:h-7 max-sm:text-tiny max-sm:rounded-small',
+            cursor: 'max-sm:rounded-small'
+          }}
+        >
+          {
+            transformEnumToJSX((key, value) => (
+              <Tab key={key} title={value} />
+            ), Weekday)
+          }
+        </Tabs>
+        <OptionsMenu
+          ariaLabel="sort-menu"
+          selectedKeys={[sort]}
+          endContent={<div className="i-mdi-filter-outline text-sm min-w-unit-3" />}
+          triggerContent="排序"
+        >
+          {
+            Object.entries(sortOptions).map(([key, value]) => (
+              <DropdownItem
+                key={key}
+                aria-label={key}
+                value={key}
+                onClick={() => router.push(`/calendar?day=${day}&sort=${key}`)}
+              >
+                {value}
+              </DropdownItem>
+            ))
+          }
+        </OptionsMenu>
+      </div>
 
       {
         isLoading || !subjects
           ? <Loading />
           : (
             <motion.div
-              key={date}
+              key={day}
               animate={{ opacity: 1 }}
               initial={{ opacity: 0 }}
               transition={{ ease: 'easeInOut', duration: 0.3 }}
@@ -73,6 +120,6 @@ export default function Calendar() {
             </motion.div>
           )
       }
-    </div>
+    </>
   );
 }
---
+++ components/options-menu/index.tsx
@@ -0,0 +1,39 @@
+import { Dropdown, DropdownTrigger, Button, DropdownMenu } from '@nextui-org/react';
+
+interface Props {
+  ariaLabel: string
+  selectedKeys: string[]
+  triggerContent: React.ReactNode
+  endContent?: React.ReactNode
+  children: JSX.Element[]
+}
+
+export default function OptionsMenu({ ariaLabel, selectedKeys, triggerContent, endContent, children }: Props) {
+  return (
+    <Dropdown
+      radius="sm"
+      classNames={{
+        content: 'min-w-[6rem] text-center'
+      }}
+    >
+      <DropdownTrigger>
+        <Button
+          radius="sm"
+          variant="bordered"
+          endContent={endContent}
+          className="max-sm:px-unit-3 max-sm:min-w-unit-16 max-sm:h-unit-8 max-sm:text-tiny max-sm:gap-unit-2"
+        >
+          {triggerContent}
+        </Button>
+      </DropdownTrigger>
+      <DropdownMenu
+        aria-label={ariaLabel}
+        variant="light"
+        selectedKeys={selectedKeys}
+        selectionMode="single"
+      >
+        {children}
+      </DropdownMenu>
+    </Dropdown>
+  );
+}
