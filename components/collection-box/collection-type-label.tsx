8f6dc0092eeb5fa39dfc997a8409444b19b93e97
kahosan <15116355657l@gmail.com>
2023-11-29 15:07:46 +0800

    chore(type-label): color

--- components/collection-box/collection-type-label.tsx
+++ components/collection-box/collection-type-label.tsx
@@ -16,11 +16,11 @@ export default function CollectionTypeLabel({ userCollection }: Props) {
         'absolute left-[12px] top-3 py-0.5 px-2.5 rounded-sm rounded-tl-md rounded-br-md text-sm font-medium text-white',
         'drop-shadow-[1px_1px_2px_black]',
         match(userCollection?.type)
-          .with(1, () => 'bg-pink-500')
-          .with(2, () => 'bg-fuchsia-500')
+          .with(1, () => 'bg-red-500')
+          .with(2, () => 'bg-blue-500')
           .with(3, () => 'bg-indigo-500')
-          .with(4, () => 'bg-emerald-500')
-          .with(5, () => 'bg-sky-500')
+          .with(4, () => 'bg-orange-500')
+          .with(5, () => 'bg-stone-500')
           .otherwise(() => 'bg-gray-500')
       )}
     >
